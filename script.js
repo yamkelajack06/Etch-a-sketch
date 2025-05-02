c642a14f5041116191095af9c437d2f97ff5b851
Yamkela Jack <yamkelajack06@gmail.com>
2025-05-02 16:03:49 +0200

    clean up and organize createGrid function for better readability

--- script.js
+++ script.js
@@ -4,65 +4,71 @@ let isDrawing = false;
 function createGrid(gridSize) {
   if (gridSize < 1) {
     alert("Value must be greater than or equal to 1");
-  } else if (gridSize > 50) {
-    alert("Value must be less than or equal to 50");
-  } else {
-    grid.innerHTML = "";
-    isDrawing = false;
-    for (let i = 0; i < gridSize; i++) {
-      grid.style.display = "flex";
-      grid.style.flexDirection = "column";
-      const row = document.createElement("div");
-      row.style.outline = "1px solid black";
-      row.style.flex = "1";
-      for (let j = 0; j < gridSize; j++) {
-        const column = document.createElement("div");
-        row.style.display = "flex";
-        column.style.flex = "1";
-        column.style.outline = "1px solid black";
-        row.appendChild(column);
-
-        column.addEventListener("mousedown", () => {
-          isDrawing = true;
-          column.style.backgroundColor = "blue";
-        });
-
-        column.addEventListener("mousemove", () => {
-          if (isDrawing) {
-            column.style.backgroundColor = "blue";
-          }
-        });
-      }
-      grid.appendChild(row);
-    }
+    return;
   }
-  isFunctionCalled = true;
-  const clearButton = document.querySelector('#clear-button');
-  clearButton.addEventListener('click', () => {
+
+  if (gridSize > 50) {
+    alert("Value must be less than or equal to 50");
+    return;
+  }
+
+  grid.innerHTML = "";
+  isDrawing = false;
+  grid.style.display = "flex";
+  grid.style.flexDirection = "column";
+
+  for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.outline = "1px solid black";
+    row.style.flex = "1";
+
+    for (let j = 0; j < gridSize; j++) {
+      const column = document.createElement("div");
+      column.style.flex = "1";
+      column.style.outline = "1px solid black";
+      row.appendChild(column);
+
+      column.addEventListener("mousedown", () => {
+        isDrawing = true;
+        column.style.backgroundColor = "blue";
+      });
+
+      column.addEventListener("mousemove", () => {
+        if (isDrawing) {
+          column.style.backgroundColor = "blue";
+        }
+      });
+    }
+
+    grid.appendChild(row);
+  }
+
+  const clearButton = document.querySelector("#clear-button");
+  clearButton.addEventListener("click", () => {
     clearGrid();
   });
 
   const clearGrid = () => {
     const columns = grid.querySelectorAll("div div");
     columns.forEach(column => {
-      column.style.backgroundColor = 'white';
+      column.style.backgroundColor = "white";
     });
-  }
+  };
 
+  isFunctionCalled = true;
 }
 
 grid.addEventListener("mouseup", () => {
   isDrawing = false;
 });
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   createGrid(2);
 });
 
-const sizeButton = document.querySelector('#size-button');
-sizeButton.addEventListener('click', () => {
-  const gridSize = document.querySelector('#grid-size').value;
-  createGrid(gridSize); 
-})
-
-
+const sizeButton = document.querySelector("#size-button");
+sizeButton.addEventListener("click", () => {
+  const gridSize = document.querySelector("#grid-size").value;
+  createGrid(gridSize);
+});
